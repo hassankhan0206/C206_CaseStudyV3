d54aa1d6a252b25538d8d3b9e973931ba31dbdfe
cokevanilla <22021136@myrp.edu.sg>
2023-08-15 12:16:35 +0800

    Junit test code (8 tests)

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
 
 public class C206_CaseStudyTest {
 
@@ -19,5 +20,106 @@ public class C206_CaseStudyTest {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
+	
+	@Test
+	public void testViewAppStatus() {
+		ArrayList<Application> appList = new ArrayList<>();
+		
+		appList.add(new Application("Wafiy", "NPCC", "Pending"));
+		appList.add(new Application("Nicholas", "Girls Guides", "Pending"));
+		appList.add(new Application("Hassan", "Girls Guides", "Pending"));
+		
+		C206_CaseStudy.viewAppStatus(appList);
+		
+		assertTrue("testViewAppStatus", true);
+		
+		
+	}
+	
+	@Test
+    public void testAppStatusApproveAndRemove() {
+        ArrayList<Application> appList = new ArrayList<>();
+        appList.add(new Application("Wafiy", "NPCC", "Pending"));
+
+        C206_CaseStudy.appStatus(appList);
+        assertEquals(0, appList.size());
+    }
+
+    @Test
+    public void testAppStatusRejectAndKeep() {
+        ArrayList<Application> appList = new ArrayList<>();
+    
+        appList.add(new Application("Nicholas", "Girls Guides", "Pending"));
+
+        C206_CaseStudy.appStatus(appList);
+        assertEquals("Rejected", appList.get(0).getStatus());
+        assertEquals(1, appList.size());
+    }
+
+    @Test
+    public void testAppStatusInvalidStudent() {
+        ArrayList<Application> appList = new ArrayList<>();
+        
+        appList.add(new Application("Wafiy", "NPCC", "Pending"));
+
+        C206_CaseStudy.appStatus(appList);
+        assertEquals(1, appList.size());
+    }
+    
+    @Test
+    public void testUpdateUserValid() {
+        ArrayList<User> userList = new ArrayList<>();
+
+        userList.add(new User("admin", "adminpass", "Admin"));
+        userList.add(new User("teacher", "teacherpass", "Teacher"));
+
+        C206_CaseStudy.updateUser(userList);
+
+        assertEquals("newpass", userList.get(0).getPassword());
+        assertEquals("Teacher", userList.get(0).getRole());
+    }
+    
+    @Test
+    public void testUpdateUserInvalidName() {
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("admin", "adminpass", "Admin"));
+        userList.add(new User("teacher", "teacherpass", "Teacher"));
+
+        
+        Helper.setMockUserInput("invaliduser", "newpass", "Student");
+
+        C206_CaseStudy.updateUser(userList);
+
+    }
+    
+    @Test
+    public void testDoDeleteUserValid() {
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("admin", "adminpass", "Admin"));
+        userList.add(new User("teacher", "teacherpass", "Teacher"));
+
+        boolean result = C206_CaseStudy.doDeleteUser(userList, "admin");
+
+        assertTrue(result);
+        assertEquals(1, userList.size());
+        assertEquals("teacher", userList.get(0).getUsername());
+    }
+    
+    @Test
+    public void testDeleteUserValid() {
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("admin", "adminpass", "Admin"));
+        userList.add(new User("teacher", "teacherpass", "Teacher"));
+
+        C206_CaseStudy.deleteUser(userList);
+
+
+        assertEquals(1, userList.size());
+        assertEquals("teacher", userList.get(0).getUsername());
+
+    }
+
+    
+
 
 }
